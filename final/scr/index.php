cdb74ea6a08f84cb20d741c329f662154c265015
rikuto <rikuriku.shin@gmail.com>
2024-01-09 14:54:11 +0900

    ログイン画面サンプル記述

--- final/scr/index.php
+++ final/scr/index.php
@@ -3,10 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,700">
+    <title>水平思考クイズサイト</title>
 </head>
 <body>
+<div id="login">
+    <form name='form-login'>
+        <span class="fontawesome-user"></span>
+        <input type="text" id="user" placeholder="Username">
     
+        <span class="fontawesome-lock"></span>
+        <input type="password" id"pass" placeholder="Password">
+            
+        <input type="submit" value="Login">
+</form>
     
 </body>
 </html>
