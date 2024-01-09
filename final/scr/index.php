4704ac49c31ed900d0f7649406567295dd4514d7
rikuto <rikuriku.shin@gmail.com>
2024-01-09 15:00:56 +0900

    パスの小修正

--- final/scr/index.php
+++ final/scr/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/coomon/css/login.css">
+    <link rel="stylesheet" href="/common/css/login.css">
     <title>水平思考クイズサイト</title>
 </head>
 <body>
