e55eb1bf3eac6fae3b3c7a5be05362996622d5a7
rikuto <rikuriku.shin@gmail.com>
2024-01-12 11:55:37 +0900

    headerにコード追加移動

--- final/src/common/php/header.php
+++ final/src/common/php/header.php
@@ -32,5 +32,12 @@ echo '<div id="header_center">';
 
 echo '</div>';
 
+echo '    <!-- 問題検索部分 -->
+<div id=quiz_search>
+    <div class="btn-border-gradient-wrap btn-border-gradient-wrap--gold">
+        <a href="" class="btn btn-border-gradient"><span class="btn-text-gradient--gold">問題に挑戦</span></a>
+    </div>
+</div>';
+
 
 ?>
--- final/src/home.php
+++ final/src/home.php
@@ -14,12 +14,7 @@
     </header>
     
 
-    <!-- 問題検索部分 -->
-    <div id=quiz_search>
-        <div class="btn-border-gradient-wrap btn-border-gradient-wrap--gold">
-        <a href="" class="btn btn-border-gradient"><span class="btn-text-gradient--gold">問題に挑戦</span></a>
-        </div>
-    </div>
+
 
 
     <footer>
