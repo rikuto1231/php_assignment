3c3b1b5f2cadfb6d53c8ab78ec632b003b55e33f
rikuto <rikuriku.shin@gmail.com>
2024-01-12 09:47:51 +0900

    headerメニュー追加

--- final/src/common/php/header.php
+++ final/src/common/php/header.php
@@ -11,12 +11,17 @@ echo '<div id="header_top">
 //headerの2層目。メニュー部分
 echo '<div id="header_center">';
 
+    echo '<p>ルール確認</p>';
+
     // 2層目の中央div
     echo '<div id="header_in_center">';
 
     echo '<a href="#"><img id="signup_img" src="common/img/logo1.png" width="150px" height="150px"></a>';
 
     echo '</div>';
+
+    echo '<p>出題する</p>';
+
 echo '</div>';
 
 
