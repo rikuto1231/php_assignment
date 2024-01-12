e2cb19c27c85ed9f48c7806d4330d94e08bd33c6
rikuto <rikuriku.shin@gmail.com>
2024-01-12 12:35:02 +0900

    問題表示部分の作成

---
+++ final/src/common/php/quiz_search.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+
+require 'DB.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+
+    // 後で詳細検索追加するならこの部分
+
+    // 問題取得処理
+    try {
+        $pdo = getDatabaseConnection();
+    
+        $sql = "SELECT * FROM question_info";
+
+        $stmt = $pdo->query($sql);
+
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        // 表示を整えて出力
+
+
+        // 
+
+
+    } catch (PDOException $e) {
+        die('Error: ' . $e->getMessage());
+    } finally {
+        // データベース接続を閉じる
+        $pdo = null;
+    }
+
+}
+?>
--- final/src/home.php
+++ final/src/home.php
@@ -14,8 +14,14 @@
     </header>
     
 
+    <!-- 問題表示部分 -->
+    <?php require 'common/php/quiz_search.php'; ?>
 
-
+    <div id="quiz_body">
+        <div id="quiz_area">
+            
+        </div>
+    </div>
 
     <footer>
     <!-- 読み込み方式 -->
