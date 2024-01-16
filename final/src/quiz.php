da8a3fa08dadc8e67a11ef4b262e553143f3f352
rikuto <rikuriku.shin@gmail.com>
2024-01-16 12:39:02 +0900

    非表示化

--- final/src/quiz.php
+++ final/src/quiz.php
@@ -11,7 +11,7 @@
 
     <script>
         function answer() {
-            document.getElementById("answer").style.display = "block";
+            document.getElementById("output_answer").style.display = "block";
         }
     </script>
 
