31fccca577bd1d703b56f2b84ed9e63564d90043
rikuto <rikuriku.shin@gmail.com>
2024-01-16 11:00:33 +0900

    cssの調整

--- final/src/quiz.php
+++ final/src/quiz.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="common/css/home.css">
+    <link rel="stylesheet" href="common/css/quiz.css">
     <link rel="stylesheet" href="common/css/header.css">
     <title>水平思考クイズサイト</title>
 </head>
@@ -15,6 +15,11 @@
 
     <!-- 問題詳細出力 -->
     <?php require 'common/php/quiz_detail.php'; ?>
+
+
+    <div id="output_area">
+    </div>
+
 
 
     <footer>
