5383e238aadf7b3f940da214192e42fbe780fa23
rikuto <rikuriku.shin@gmail.com>
2024-01-19 18:00:39 +0900

    修正

---
+++ final/src/mypage.php
@@ -0,0 +1,39 @@
+<? session_start();?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="common/css/home.css">
+    <link rel="stylesheet" href="common/css/header.css">
+    <title>水平思考クイズサイト</title>
+</head>
+<body>
+
+
+    <header>
+    <!-- 読み込み方式 -->
+        <?php require 'common/php/header.php'; ?>
+    </header>
+
+    <div class="btn-border-gradient-wrap btn-border-gradient-wrap--gold">
+        <a href="home.php" class="btn btn-border-gradient">
+            <span class="btn-text-gradient--gold">投稿問題の更新</span>
+        </a>
+    </div>
+
+    <div class="btn-border-gradient-wrap btn-border-gradient-wrap--gold">
+        <a href="" class="btn btn-border-gradient">
+            <span class="btn-text-gradient--gold">投稿問題の削除</span>
+        </a>
+    </div>
+
+
+
+
+    <footer>
+    <!-- 読み込み方式 -->
+    </footer>
+
+</body>
+</html>
