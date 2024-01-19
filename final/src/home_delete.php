3ee867ff13cbb2ef87b5f681da1a063f8a55076c
rikuto <rikuriku.shin@gmail.com>
2024-01-19 18:27:47 +0900

    修正

--- final/src/home_delete.php
+++ final/src/home_delete.php
@@ -17,53 +17,10 @@
     </header>
 
     <h1>削除画面</h1>
-<?php session_start();
 
-require 'common/php/DB.php';
+    <!-- 問題表示部分 -->
+    <?php require 'common/php/quiz_user.php'; ?>
 
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-
-    // ユーザID取得
-    if (isset($_SESSION["user_id"])) {
-        $user_id = $_SESSION["user_id"];
-
-        // 問題取得処理
-        try {
-            $pdo = getDatabaseConnection();
-
-            $sql = "SELECT * FROM question_info WHERE user_id = :user_id";
-
-            $stmt = $pdo->prepare($sql);
-            $stmt->bindParam(":user_id", $user_id);
-            $stmt->execute();
-
-            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-            // 表示を整えて出力
-            echo '<div id="quiz_body">';
-            echo '<div id="quiz_area">';
-
-            foreach($result as $row){
-                echo '<div class="quiz_out">';
-                echo '<h2><a href="quiz_delete.php?quiz='.$row['question_id'].'">'.$row['title'].'</a></h2>';
-                echo '</div>';
-            }
-
-            echo '</div>';
-            echo '</div>';
-
-        } catch (PDOException $e) {
-            die('Error: ' . $e->getMessage());
-        } finally {
-            // データベース接続を閉じる
-            $pdo = null;
-        }
-    } else {
-        echo "ユーザーIDが取得できません。";
-    }
-}
-
-?>
 
     <footer>
     <!-- 読み込み方式 -->
