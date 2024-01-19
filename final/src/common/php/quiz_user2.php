0a5e644416304985534cfc3f9556b9b9d454e8cf
rikuto <rikuriku.shin@gmail.com>
2024-01-19 19:11:01 +0900

    修正

---
+++ final/src/common/php/quiz_user2.php
@@ -0,0 +1,49 @@
+<?php
+// セッションが開始されていない場合のみ開始する
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+require 'DB.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    // ユーザID取得
+    if (isset($_SESSION["user_id"])) {
+        $user_id = $_SESSION["user_id"];
+
+        // 問題取得処理
+        try {
+            $pdo = getDatabaseConnection();
+
+            $sql = "SELECT * FROM question_info WHERE user_id = :user_id";
+
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(":user_id", $user_id);
+            $stmt->execute();
+
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            // 表示を整えて出力
+            echo '<div id="quiz_body">';
+            echo '<div id="quiz_area">';
+
+            foreach($result as $row){
+                echo '<div class="quiz_out">';
+                echo '<h2><a href="question_up.php?quiz='.$row['question_id'].'">'.$row['title'].'</a></h2>';
+                echo '</div>';
+            }
+
+            echo '</div>';
+            echo '</div>';
+
+        } catch (PDOException $e) {
+            die('Error: ' . $e->getMessage());
+        } finally {
+            // データベース接続を閉じる
+            $pdo = null;
+        }
+    } else {
+        echo "ユーザーIDが取得できません。";
+    }
+}
+?>
--- final/src/home_up.php
+++ final/src/home_up.php
@@ -1,4 +1,4 @@
-<? session_start();?>
+<?php session_start();?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -17,54 +17,10 @@
     </header>
 
     <h1>削除画面</h1>
-<?
 
-require 'DB.php';
+    <!-- 問題表示部分 -->
+    <?php require 'common/php/quiz_user2.php'; ?>
 
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-
-    // ユーザID取得
-    session_start();
-    if (isset($_SESSION["user_id"])) {
-        $user_id = $_SESSION["user_id"];
-
-        // 問題取得処理
-        try {
-            $pdo = getDatabaseConnection();
-
-            $sql = "SELECT * FROM question_info WHERE user_id = :user_id";
-
-            $stmt = $pdo->prepare($sql);
-            $stmt->bindParam(":user_id", $user_id);
-            $stmt->execute();
-
-            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-            // 表示を整えて出力
-            echo '<div id="quiz_body">';
-            echo '<div id="quiz_area">';
-
-            foreach($result as $row){
-                echo '<div class="quiz_out">';
-                echo '<h2><a href="common/php/quiz_delete.php?quiz='.$row['question_id'].'">'.$row['title'].'</a></h2>';
-                echo '</div>';
-            }
-
-            echo '</div>';
-            echo '</div>';
-
-        } catch (PDOException $e) {
-            die('Error: ' . $e->getMessage());
-        } finally {
-            // データベース接続を閉じる
-            $pdo = null;
-        }
-    } else {
-        echo "ユーザーIDが取得できません。";
-    }
-}
-
-?>
 
     <footer>
     <!-- 読み込み方式 -->
